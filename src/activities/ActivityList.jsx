ff6354029049b020e5886be04056f79aecf06b91
Ricky <gomez.ricky@gmail.com>
2025-11-19 10:45:48 -0600

    Completed FitnessTracker  Workshop

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,9 +1,42 @@
-export default function ActivityList({ activities }) {
+import { useState } from "react";
+import { deleteActivity } from "../api/activities";
+import { useAuth } from "../auth/AuthContext";
+
+export default function ActivityList({ activities, syncActivities }) {
   return (
     <ul>
       {activities.map((activity) => (
-        <li key={activity.id}>{activity.name}</li>
+        <ExerciseListItem
+          key={activity.id}
+          activity={activity}
+          syncActivities={syncActivities}
+        />
       ))}
     </ul>
+  );
+}
+
+function ExerciseListItem({ activity, syncActivities }) {
+  const { token } = useAuth();
+
+  const [error, setError] = useState(null);
+
+  const tryDelete = async () => {
+    setError(null);
+
+    try {
+      await deleteActivity(token, activity.id);
+      syncActivities();
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <li>
+      <p>{activity.name}</p>
+      {token && <button onClick={tryDelete}>Delete</button>}
+      {error && <p role="alert">{error}</p>}
+    </li>
   );
 }
